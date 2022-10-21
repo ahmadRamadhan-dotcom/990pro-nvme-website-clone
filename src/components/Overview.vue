729952296f9f0746a3140c4e1aaefc057f4cb4b1
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-21 17:30:15 +0700

    chore: responsive overview section for mobile devices

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,15 +1,17 @@
 <template>
   <div
-    class="bg-[#000000] sm:h-[30vh] md:h-[30vh] lg:h-[45vh] flex justify-center items-center"
+    class="bg-[#000000] h-[35vh] sm:h-[30vh] md:h-[30vh] lg:h-[45vh] flex justify-center items-center"
   >
-    <div class="grid sm:place-content-center sm:place-items-center">
+    <div
+      class="grid place-content-center place-items-center sm:place-content-center sm:place-items-center"
+    >
       <h2
-        class="sm:text-2xl md:text-[34px] lg:text-5xl text-center font-['Samsung_Sharp_Sans'] font-bold"
+        class="text-2xl sm:text-2xl md:text-[34px] lg:text-5xl text-center font-['Samsung_Sharp_Sans'] font-bold"
       >
         The Ultimate SSD
       </h2>
       <span
-        class="w-11/12 mt-8 text-center font-normal font-['SamsungOne'] sm:text-sm md:text-[15px] lg:text-lg xl:hidden"
+        class="w-11/12 mt-8 text-center font-normal font-['SamsungOne'] text-sm sm:text-sm md:text-[15px] lg:text-lg xl:hidden"
       >
         Reach max performance of PCIe<sup>®</sup> 4.0. Experience
         longer-lasting, opponent-blasting speed The in-house controller's smart
@@ -21,11 +23,11 @@
         <span
           v-for="(data, index) in overviewData"
           :key="index"
-          class="sm:hidden xl:block font-normal font-['SamsungOne'] text-lg"
+          class="hidden sm:hidden xl:block font-normal font-['SamsungOne'] text-lg"
           >{{ data.content }}
         </span>
         <span
-          class="sm:hidden md:block font-normal font-['SamsungOne'] text-[12px] md:mt-4 xl:mt-8 text-[#f8f8f8]"
+          class="hidden sm:hidden md:block font-normal font-['SamsungOne'] text-[12px] md:mt-4 xl:mt-8 text-[#f8f8f8]"
         >
           * PCIe 4.0’s best theoretical sequential read is 8000 MB/s - 990 PRO
           reaches 7,450 MB/s as of Q3, 2022.
