ad85bb63bbf7034d24da9080f98074a1f8dac319
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-22 06:03:43 +0700

    fix: make code better in pcie express x4 speed section

--- src/components/NoticeTesPerf.vue
+++ src/components/NoticeTesPerf.vue
@@ -2,26 +2,8 @@
   <div
     class="leading-3 xl:grid text-center md:leading-5 mt-8 sm:pb-0 md:pb-10 grid place-content-center place-items-center sm:grid sm:place-content-center sm:place-items-center lg:mt-8"
   >
-    <span
-      v-for="(data, index) in noticeDataTopLargeDevices"
-      :key="index"
-      class="hidden sm:hidden lg:block font-['SamsungOne'] font-normal text-[#f8f8f8] text-[12px]"
-    >
-      {{ data.content }}
-    </span>
-
-    <span
-      class="hidden sm:grid md:hidden font-['SamsungOne'] w-11/12 font-normal text-[#f8f8f8] text-[12px]"
-    >
-      * Sequential and random write performance was measured with Intelligent
-      Turbo Write technology being activated. Intelligent Turbo Write operates
-      only within a specific data transfer size. Performance may vary depending
-      on SSD’s firmware, system hardware &amp; configuration and other factors.
-      For detailed information, please contact your local service center
-    </span>
-
     <p
-      class="w-11/12 sm:hidden font-['SamsungOne'] font-normal text-[#f8f8f8] text-[12px]"
+      class="sm:grid md:grid lg:grid font-['SamsungOne'] w-10/12 sm:w-11/12 md:w-1 lg:w-9/12 font-normal text-[#f8f8f8] text-[12px]"
     >
       * Sequential and random write performance was measured with Intelligent
       Turbo Write technology being activated. Intelligent Turbo Write operates
@@ -30,41 +12,13 @@
       For detailed information, please contact your local service center
     </p>
 
-    <span
-      class="hidden sm:hidden md:grid lg:hidden font-['SamsungOne'] w-10/12 font-normal text-[#f8f8f8] text-[12px]"
-    >
-      * Sequential and random write performance was measured with Intelligent
-      Turbo Write technology being activated. Intelligent Turbo Write operates
-      only within a specific data transfer size. Performance may vary depending
-      on SSD’s firmware, system hardware &amp; configuration and other factors.
-      For detailed information, please contact your local service center
-    </span>
-
     <div
       class="grid text-center place-content-center place-items-center mt-4 sm:mt-4 xl:mt-4"
     >
       <span
-        v-for="(data, index) in noticeDataBottomLargeDevices"
-        :class="data.id === 3 ? 'xl:mt-5' : ''"
+        v-for="(data, index) in noticeData"
         :key="index"
-        class="hidden sm:hidden font-['SamsungOne'] font-normal text-[#f8f8f8] text-[12px]"
-      >
-        {{ data.content }}
-      </span>
-
-      <span
-        v-for="(data, index) in noticeDataBottomSmallDevices"
-        :key="index"
-        :class="data.id === 3 ? 'mt-4' : ''"
-        class="hidden sm:grid font-['SamsungOne'] font-normal text-[#f8f8f8] text-[12px]"
-      >
-        {{ data.content }}
-      </span>
-
-      <span
-        v-for="(data, index) in noticeDataBottomMobileDevices"
-        :key="index"
-        class="w-10/12 sm:hidden font-['SamsungOne'] mb-4 font-normal text-[#f8f8f8] text-[12px]"
+        class="w-10/12 sm:w-[95%] md:w-9/12 sm:grid font-['SamsungOne'] mb-4 font-normal text-[#f8f8f8] text-[12px]"
       >
         {{ data.content }}
       </span>
@@ -78,42 +32,7 @@
 </template>
 
 <script setup>
-const noticeDataTopLargeDevices = [
-  {
-    id: 1,
-    content:
-      " * Sequential and random write performance was measured Intelligent Turbo Write technology being activated. Intelligent Turbo Write operates only within a specific data transfer size.",
-  },
-  {
-    id: 2,
-    content:
-      "Performance may vary depending on SSD’s firmware, system hardware & configuration and other factors. For detailed information, please contact your local service center",
-  },
-];
-
-const noticeDataBottomSmallDevices = [
-  {
-    id: 1,
-    content:
-      "* Test system configuration: AMD Ryzen 7 5800X 8-Core Processor CPU@3.80GHz, DDR4 3600MHz 16GBx2 (PC4- ",
-  },
-  {
-    id: 2,
-    content:
-      "25600 Overclock), OS - Windows 10 Pro 64bit, Chipset - ASRock-X570 Taichi",
-  },
-  {
-    id: 3,
-    content:
-      "* To maximize the performance of the 990 PRO, please check whether your system supports PCIe ® 4.0 at the ",
-  },
-  {
-    id: 4,
-    content: "Intel or AMD website.",
-  },
-];
-
-const noticeDataBottomMobileDevices = [
+const noticeData = [
   {
     id: 1,
     content:
@@ -123,20 +42,6 @@ const noticeDataBottomMobileDevices = [
     id: 2,
     content:
       "* To maximize the performance of the 990 PRO, please check whether your system supports PCIe® 4.0 at the Intel or AMD website.",
-  },
-];
-
-const noticeDataBottomLargeDevices = [
-  {
-    id: 1,
-    content:
-      "* Test system configuration: AMD Ryzen 7 5800X 8-Core Processor CPU@3.80GHz, DDR4 3600MHz 16GBx2 (PC4-25600 Overclock),",
-  },
-  { id: 2, content: "OS - Windows 10 Pro 64bit, Chipset - ASRock-X570 Taich" },
-  {
-    id: 3,
-    content:
-      "* To maximize the performance of the 990 PRO, please check whether your system supports PCIe®4.0 at the Intel or AMD website.",
   },
 ];
 </script>
