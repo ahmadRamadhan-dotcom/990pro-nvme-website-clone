1e68d9f886178a6a00a44784ec1cd9fe319913d7
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-22 08:34:22 +0700

    chore: responsive thermal solution section page in mobile devices

--- src/components/ThermalSolution.vue
+++ src/components/ThermalSolution.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="xl:flex xl:justify-center">
     <div
-      class="xl:w-full sm:flex sm:flex-col-reverse sm:justify-center sm:text-center md:text-start sm:items-center md:flex md:flex-row md:justify-center md:items-center"
+      class="xl:w-full flex flex-col-reverse justify-center text-center sm:flex sm:flex-col-reverse sm:justify-center sm:text-center md:text-start sm:items-center md:flex md:flex-row md:justify-center md:items-center"
     >
       <img
-        class="md:h-[450px] lg:h-[580px] xl:h-[96%]"
+        class="mt-12 sm:mt-0 md:h-[450px] lg:h-[580px] xl:h-[96%]"
         src="../assets/990-pro-of-pc-smart-thermal_.webp"
         alt="displaying image how smart thermal solution smasung work"
       />
       <div
-        class="sm:mt-10 lg:mt-32 xl:mt-[110px] sm:grid sm:place-content-center sm:place-items-center md:place-items-start md:place-content-start xl:w-[45%]"
+        class="mt-12 sm:mt-10 lg:mt-32 xl:mt-[110px] grid place-content-center place-items-center sm:grid sm:place-content-center sm:place-items-center md:place-items-start md:place-content-start xl:w-[45%]"
       >
         <span
-          class="sm:text-2xl md:text-[34px] lg:text-5xl md:w-9/12 xl:w-8/12 font-bold font-['Samsung_Sharp_Sans']"
+          class="text-2xl sm:text-2xl md:text-[34px] lg:text-5xl md:w-9/12 xl:w-8/12 font-bold font-['Samsung_Sharp_Sans']"
           >Smart thermal solution</span
         >
         <span
-          class="sm:mt-6 font-['SamsungOne'] font-normal sm:text-[15px] lg:text-lg sm:w-11/12 md:w-10/12 lg:w-11/12 xl:w-10/12"
+          class="mt-5 sm:mt-6 font-['SamsungOne'] font-normal text-[15px] sm:text-[15px] lg:text-lg w-10/12 sm:w-11/12 md:w-10/12 lg:w-11/12 xl:w-10/12"
         >
           Speed beyond the heat.<br class="hidden sm:hidden xl:block" />
           The nickel-coated controller and cutting-edge thermal control
