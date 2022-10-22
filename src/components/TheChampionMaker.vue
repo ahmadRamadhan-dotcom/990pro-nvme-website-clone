24faa3071e8d92ef96582674afce97cabb5578fe
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-22 09:28:17 +0700

    chore: responsive the champion maker section for mobile devices

--- src/components/TheChampionMaker.vue
+++ src/components/TheChampionMaker.vue
@@ -1,34 +1,38 @@
 <template>
-  <div class="sm:mt-12 lg:mt-20 xl:mt-20 flex justify-center items-center">
+  <div
+    class="mt-20 sm:mt-12 lg:mt-20 xl:mt-20 flex justify-center items-center"
+  >
     <div class="grid place-content-center place-items-center text-center">
       <span
-        class="font-['Samsung_Sharp_Sans'] font-bold sm:text-2x md:text-[34px] lg:text-5xl"
+        class="font-['Samsung_Sharp_Sans'] font-bold text-2xl sm:text-2xl md:text-[34px] lg:text-5xl"
         >The champion maker</span
       >
       <span
-        class="sm:mt-4 xl:mt-7 sm:text-[14px] md:text-[15px] lg:text-lg text-center sm:w-11/12 md:w-9/12 lg:w-8/12 xl:w-6/12 font-normal font-['SamsungOne']"
+        class="mt-4 sm:mt-4 xl:mt-7 text-sm sm:text-[14px] md:text-[15px] lg:text-lg text-center w-11/12 sm:w-11/12 md:w-9/12 lg:w-8/12 xl:w-6/12 font-normal font-['SamsungOne']"
       >
         The best gameplay experience.
         <br class="hidden sm:hidden md:block xl:block" />
         A more than 55% improvement in random performance enables faster loads
         for ultimate gaming realism on PS5 and DirectStorage PC games.
       </span>
-      <div class="sm:flex sm:flex-col-reverse md:flex-col xl:mt-6">
-        <div class="grid place-content-center place-items-center sm:mt-6">
+      <div
+        class="flex flex-col-reverse sm:flex sm:flex-col-reverse md:flex-col xl:mt-6"
+      >
+        <div class="grid place-content-center place-items-center mt-6 sm:mt-6">
           <span
-            class="font-['SamsungOne'] sm:w-11/12 md:w-9/12 lg:w-full text-[12px] font-normal text-[#f8f8f8]"
+            class="font-['SamsungOne'] w-11/12 sm:w-11/12 md:w-9/12 lg:w-full text-[12px] font-normal text-[#f8f8f8]"
           >
             * DirectStorage technology from Microsoft loads games faster than
             before by leveraging the multiple GB/sec speed of modern NVMe SSDs.
           </span>
           <span
-            class="sm:mt-5 xl:mt-5 font-['SamsungOne'] text-[12px] font-normal text-[#f8f8f8]"
+            class="mt-5 sm:mt-5 xl:mt-5 font-['SamsungOne'] text-[12px] font-normal text-[#f8f8f8]"
           >
             * Results are based on a comparison with Samsung 980 PRO
           </span>
         </div>
         <img
-          class="sm:mt-12 xl:mt-12"
+          class="mt-12 sm:mt-12 xl:mt-12"
           src="../assets/990-of-pc-gaming-benefit.webp"
           alt="the image of the benefit using samsung 990 pro nvme if you are gamers"
         />
