5f8866e325714c02780fa3a0088c176a4ce8eb5b
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-22 09:23:49 +0700

    chore: responsive the champion maker section for small devices, medium devices, large devices and extra large devices

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import Pciex4PerfSpeed from "./components/Pciex4PerfSpeed.vue";
 import Samsung990ProImage from "./components/990ProImage.vue";
 import PowerEfficiency from "./components/PowerEfficiency.vue";
 import ThermalSolution from "./components/ThermalSolution.vue";
+import TheChampionMaker from "./components/TheChampionMaker.vue";
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 </script>
@@ -18,4 +19,5 @@ import ThermalSolution from "./components/ThermalSolution.vue";
   <Pciex4PerfSpeed />
   <PowerEfficiency />
   <ThermalSolution />
+  <TheChampionMaker />
 </template>
