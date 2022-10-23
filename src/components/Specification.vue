164877ff621e02835e6e0fa05f8bd040e1d7d126
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-23 12:45:04 +0700

    fix: specifications section not responsive for mobile devices

--- src/components/Specification.vue
+++ src/components/Specification.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    class="bg-[#1a1a1c] flex justify-center sm:pb-32 xl:pb-24 mt-20 sm:mt-20 xl:mt-36"
+    class="bg-[#1a1a1c] flex justify-center pb-20 sm:pb-32 xl:pb-24 mt-20 sm:mt-20 xl:mt-36"
   >
     <div
-      class="grid place-content-center sm:w-11/12 md:w-full xl:w-full place-items-center sm:mt-20 xl:mt-16"
+      class="grid place-content-center sm:w-11/12 md:w-full xl:w-full place-items-center mt-20 sm:mt-20 xl:mt-16"
     >
       <span
-        class="font-['Samsung_Sharp_Sans'] font-bold sm:text-2xl md:text-[28px] lg:text-4xl"
+        class="font-['Samsung_Sharp_Sans'] font-bold text-2xl sm:text-2xl md:text-[28px] lg:text-4xl"
         >Specifications
       </span>
       <div
-        class="sm:mt-20 xl:mt-24 sm:grid sm:grid-cols-2 sm:gap-24 md:flex lg:gap-[100px] xl:flex xl:gap-[160px]"
+        class="mt-20 sm:mt-20 xl:mt-24 grid grid-cols-2 gap-16 sm:grid sm:grid-cols-2 sm:gap-24 md:flex lg:gap-[100px] xl:flex xl:gap-[160px]"
       >
         <div
           v-for="(data, index) in quickSpecData"
@@ -20,21 +20,21 @@
           <img
             :src="'/src/assets/' + data.image"
             alt=""
-            class="sm:h-10 md:h-12 xl:h-12"
+            class="h-12 sm:h-10 md:h-12 xl:h-12"
           />
           <span
-            class="sm:mt-5 xl:mt-6 font-['SamsungOne'] font-bold sm:text-[12px] md:text-[15px] lg:text-lg"
+            class="mt-5 sm:mt-5 xl:mt-6 font-['SamsungOne'] font-bold text-[12px] sm:text-[12px] md:text-[15px] lg:text-lg"
             >{{ data.name }}</span
           >
           <span
-            class="sm:mt-1 xl:mt-1 font-['SamsungOne'] font-normal sm:text-[12px] xl:text-[12px]"
+            class="mt-1 sm:mt-1 xl:mt-1 font-['SamsungOne'] font-normal text-[12px] sm:text-[12px] xl:text-[12px]"
             >{{ data.content }}</span
           >
         </div>
       </div>
       <button
         aria-label="this button for show detail specification of this nvme"
-        class="inline-flex sm:mt-10 xl:mt-12 items-center justify-center sm:py-1 sm:px-5 xl:px-5 xl:py-1.5 text-sm font-bold leading-6 font-['SamsungOne'] whitespace-no-wrap bg-white border border-gray-200 rounded-full text-[#000000] shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none"
+        class="inline-flex mt-10 sm:mt-10 xl:mt-12 items-center justify-center py-1 px-5 sm:py-1 sm:px-5 xl:px-5 xl:py-1.5 text-sm font-bold leading-6 font-['SamsungOne'] whitespace-no-wrap bg-white border border-gray-200 rounded-full text-[#000000] shadow-sm hover:bg-gray-50 focus:outline-none focus:shadow-none"
       >
         View more
       </button>
