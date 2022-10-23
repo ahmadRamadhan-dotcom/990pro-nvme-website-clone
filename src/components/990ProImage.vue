b95d3a4f6aaa88106154dc5c4b9b081f907b811a
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-23 18:20:08 +0700

    responsive banner hero and overview section for 2xl devices

--- src/components/990ProImage.vue
+++ src/components/990ProImage.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="999pro-image">
     <img
+      class="2xl:h-[90vh] 2xl:mx-auto"
       src="../assets/feature-image.jpeg"
       alt="nice looking samsung 990 pro nvme ssd"
     />
--- src/components/BannerHero.vue
+++ src/components/BannerHero.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="banner-hero"
-    class="bg-[url('/hero-image-small.jpeg')] h-[540px] sm:h-[110vh] md:h-[540px] lg:h-[83vh] xl:h-[95vh] sm:bg-[url('/hero-image-small.jpeg')] md:bg-[url('/hero-image.avif')] bg-no-repeat bg-[center_top] sm:bg-top xl:bg-[center_top] bg-cover sm:bg-cover flex justify-center"
+    class="bg-[url('/hero-image-small.jpeg')] h-[540px] sm:h-[110vh] md:h-[540px] lg:h-[83vh] xl:h-[95vh] 2xl:h-[60vh] sm:bg-[url('/hero-image-small.jpeg')] md:bg-[url('/hero-image.avif')] bg-no-repeat bg-[center_top] sm:bg-top xl:bg-[center_top] bg-cover sm:bg-cover flex justify-center"
   >
     <h1
       class="text-[32px] sm:text-[32px] md:text-5xl lg:text-7xl font-['Samsung_Sharp_Sans'] font-bold text-center lg:leading-[90px] mt-16 sm:mt-10 md:mt-20 w-[300px] lg:mt-[110px] xl:mt-24 sm:w-[300px] md:w-[400px] lg:w-[600px] xl:w-[600px]"
